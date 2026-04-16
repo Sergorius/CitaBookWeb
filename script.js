cecb7e03d6977e9398a00793ef9a4c635ed3e660
Sergorius <159528565+Sergorius@users.noreply.github.com>
2026-04-16 13:46:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+reveals.forEach((item) => observer.observe(item));
